0280016e06b4d4e59a36936e142f803b1c405147
Samuel Grisales Barrera <samuelg2197@gmail.com>
2025-11-14 19:27:11 +0000

    De hecho te iba a comentar que se interrumpió el ultimo

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,4 +1,4 @@
 // This file is intentionally left blank.
 // The Firebase configuration will be injected by the App Prototyper backend.
 // @ts-nocheck
-export const firebaseConfig = {"apiKey":"placeholder","authDomain":"placeholder","projectId":"placeholder","storageBucket":"placeholder","messagingSenderId":"placeholder","appId":"placeholder"};
+export const firebaseConfig = {"apiKey":"AIzaSyB-R_iKyS8mJ4d8X_cZ-g2j-w5sJ6jH7yE","authDomain":"studio-3199003921-53a3f.firebaseapp.com","projectId":"studio-3199003921-53a3f","storageBucket":"studio-3199003921-53a3f.appspot.com","messagingSenderId":"17263205315","appId":"1:17263205315:web:a964a7d78a870d0329068b","measurementId":"G-X5110H200G"};
