60d6d557c128dba2f284c90b64fa166f35e283a1
Samuel Grisales Barrera <samuelg2197@gmail.com>
2025-11-14 21:06:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3199003921-53a3f",
+  "appId": "1:172632343000:web:d734f701abd1dd5a14f2ad",
+  "apiKey": "AIzaSyBrHKCxfNG759ySSh3bvmEVu29iR0ROgwI",
+  "authDomain": "studio-3199003921-53a3f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "172632343000"
+};
