d7d132e7d6fc580a627e6eed115f18a005e970d1
Samuel Grisales Barrera <samuelg2197@gmail.com>
2025-11-15 18:49:22 +0000

    pOr favor, ten en cuenta la ortografía, este es el mensaje que debe apar

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -52,7 +52,7 @@ export default function LoginPage() {
         }
     };
     checkUsers();
-  }, []);
+  }, [isSetupNeeded]);
 
   const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -238,7 +238,7 @@ export default function LoginPage() {
           </CardContent>
            <CardFooter>
             <p className="text-xs text-muted-foreground text-center w-full">
-              Si aún no tienes asignado un usuario, comunicate con el administrador
+              si aún no tienen asignado un usuario, comunícate con el administrador
             </p>
           </CardFooter>
         </Card>
